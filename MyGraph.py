6db9500f115b40ed785732fdd735f8e3187013bb
Garrett Kembel <garrett_kembel1@baylor.edu>
2024-01-23 13:03:02 -0600

    updated print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,10 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+
+"step 1. Create the VE (virtual environment)"
+"mac - python3 -m venv myvenv"
+"2. activate the VE"
+"3. install 3rd party library/module"
